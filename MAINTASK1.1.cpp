19a3fae9e175b8a80951a9604137215d3f06c921
Adam2-t <Adam2.terry@live.uwe.ac.uk>
2026-04-12 13:39:18 +0100

    Create MAINTASK1.1.cpp
    Code for Main Task 1.1

---
+++ MAINTASK1.1.cpp
@@ -0,0 +1,33 @@
+#include "mbed.h"
+
+// Blinking rate in milliseconds
+#define BLINKING_RATE1     1000ms
+
+int main()
+{
+    // Initialise the digital pin LED1 as an output
+#ifdef LED1
+    DigitalOut led(LED1);
+    DigitalOut led2(LED2);
+    DigitalOut led3(LED3);
+#else
+    bool led;
+    bool led2;
+    bool led3;
+
+#endif
+
+while (true) {
+    
+    led = !led;
+    ThisThread::sleep_for(BLINKING_RATE1);
+
+    led2 = !led2;
+    ThisThread::sleep_for(BLINKING_RATE1);
+    
+
+    led3 = !led3;
+    ThisThread::sleep_for(BLINKING_RATE1); 
+    }
+}
+
